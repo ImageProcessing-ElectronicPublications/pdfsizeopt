cf716b40ab2969eec9e5ece6eeb3924ee98d8812
Rogério Brito <rbrito@ime.usp.br>
2018-11-27 23:20:43 -0200

    Multivalent: Enable more flags to make smaller sizes.

--- lib/pdfsizeopt/main.py
+++ lib/pdfsizeopt/main.py
@@ -9177,8 +9177,8 @@ class PdfData(object):
     # * Don't add -jpeg, it introduces lossy compression.
     # * Don't add -subset, it's expreimental.
     # * FYI http://code.google.com/p/pdfsizeopt/issues/detail?id=30 .
-    # multivalent_flags = '-nopagepiece -noalt -mon -nocore'
-    multivalent_flags = '-nopagepiece -noalt -mon'
+    # multivalent_flags = '-nopagepiece -noalt -mon -nowebcap -nostruct'
+    multivalent_flags = '-nopagepiece -noalt -mon -nocore -nowebcap -nostruct'
 
     # TODO(pts): Work around exception for emptypage.pdf:
     # psotmp.PID.conv.mi.tmp.pdf: java.lang.ClassCastException:
